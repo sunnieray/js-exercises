cc6b5df7a55acacfa11d19a91b84c90dbb5aeddd
sunnieray <sunnieray1@gmail.com>
2019-11-04 21:09:57 -0600

    7

--- exercises.js
+++ exercises.js
@@ -38,35 +38,62 @@ console.log(alphabet)
 console.log(`${alphabet.length} is the length og this array`)
 
 //???????// 5 create a variable of an array of numbers 1-5
-let numbers = [1, 2, 3, 4, 5]
+//let numbers = [1, 2, 3, 4, 5]
     // 5a create a "const" which uses "map" to multiply each number with itself
-const multiply = numbers.map()
+//const multiply = numbers.map()
     // 5b console.log the results of the mapped array, label the console log with string
     // 5c use the "forEach" method on the mapped array to console log the remainder when divided by 2
 
-// 6 create an variable holding an object of each person with the properties "name, age, jobTitle, laptopType, isStudent" the data can/is fictional
+// 6 create an variable holding an object of each person with the properties "name, age, jobTitle, laptopType, isStudent the data can/is fictional
 var person = [
-   {name: Sunnie,
-    age: 22,
-    job: Lyft,
-    laptop: MacBook,
-    isStudent: true},
-    {name: zack,
-    age: 27,
-    job: Lead,
-    laptop: MacBook,
-    isStudent: false},
-    {name: Sunnie,
-    age: 25,
-    job: microsoft,
-    laptop: windows,
-    isStudent: true}
-]
+   {"name" : "Sunnie",
+    "age": 22,
+    "job": "Lyft",
+    "laptop": "MacBook",
+    "pronoun": "her",
+    "isStudent": true},
+    {"name": "Zack",
+    "age": 27,
+    "job": "Lead",
+    "laptop": "MacBook",
+    "pronoun": "his",
+    "isStudent": false},
+    {"name": "Chloe",
+    "age": 25,
+    "job": "Microsoft",
+    "laptop": "Windows",
+    "pronoun": "her",
+    "isStudent": true}
+];
+let personDiv = document.getElementById("person");
+
+for(var counter = 0; counter < person.length; counter++){
+
+    var personOfMoment = person[counter];
+
+    var personName = personOfMoment.name;
+    var personAge = personOfMoment.age;
+    var personJob = personOfMoment.job;
+    var personLaptop = personOfMoment.laptop;
+    var personStudent = personOfMoment.isStudent;
+    var personPronoun = personOfMoment.pronoun;
+
+    var personDescription = 
+      `<p>${personName} is ${personAge} years old and works at ${personJob}. 
+      ${personName} uses a ${personLaptop} laptop, and ${personPronoun} student status is ${personStudent}`;
+
+      personDiv.innerHTML += `${personDescription}</div>`;
+    }
+
+
     // 6a create a for loop to dynamically create a sentence describing each "person"
-for person 
+ 
     // 6b inject these sentences in "p" elements in the html div
 
 // 7 create two "const" variables of strings. console log the combined result using "concat"
+const var1 = ["oranges", "apples", "bananas"]
+const var2 = ["grape", "peaches", "mangos"]
+console.log(var1.concat () )
 
 // 8 create an array of strings made up of words of a quote or nursery rhyme.
     // 8a use the "filter" method on the array variable to gather the words with length greater than 3
